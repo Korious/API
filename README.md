e67f7a798cbc4e407cb6e9499cc84c54c9b48aa8
korious <innovativepotencial@gmail.com>
2025-01-30 13:43:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HNG12 Public API [Week 0]
+
+## Description
+This API returns:
+- Your registered email address.
+- The current datetime in ISO 8601 format.
+- The GitHub URL of this project.
+
+## API Endpoint
+**GET** `<your-deployed-url>`
+
+### Response:
+```json
+{
+  "email": "innovativepotencial@gmail.com",
+  "current_datetime": "2025-01-30T09:30:00Z",
+  "github_url": "https://github.com/Korious/API.git"
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express;
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    email:"innovativepotencial@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url:"https://github.com/Korious/API.git",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
